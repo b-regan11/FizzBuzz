03b90865b599c5a14f491c45c150ff0ebe8f2ef5
b-regan11 <reganbill02@gmail.com>
2024-03-26 19:16:40 -0400

    If Conditionals

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,17 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        // Main method, but nothing else
+        for (int i = 1; i <= 100; i++) {
+            int divisorA = 3;
+            int divisorB = 5;
+            int remainderA = i % divisorA;
+            int remainderB = i % divisorB;
+            if (remainderA == 0 && remainderB == 0) {
+                System.out.println("FizzBuzz");
+            } else if (remainderA == 0) {
+                System.out.println("Fizz");
+            } else if (remainderB == 0) {
+                System.out.println("Buzz");
+            }
+        }
     }
 }
